b1c59869fe0a813a4985df5a7f129333aa379d93
StephBerg86 <info@stephaniebergman.nl>
2020-08-25 19:10:30 +0200

    added styling and searchbar

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getExercises } from "../../store/exercise/actions";
 import { selectMethod } from "../../store/exercise/selectors";
@@ -10,30 +10,57 @@ export default function Homepage() {
   const dispatch = useDispatch();
   const exercises = useSelector(selectMethod);
 
+  const [searchTerm, setSearchTerm] = useState();
+  const [searchResults, setSearchResults] = useState([]);
+
   useEffect(() => {
     dispatch(getExercises());
   }, [dispatch]);
 
-  console.log("exercises", exercises);
+  useEffect(() => {
+    const results = exercises.filter((exercise) =>
+      exercise.name.includes(searchTerm)
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const data = !searchTerm ? exercises : searchResults;
+
   return (
     <div>
-      <h3>Exercises</h3>
-      {exercises.map((exercise) => {
+      <h3 className="hpTitle">Exercises</h3>
+      <input
+        className="searchBar"
+        placeholder="Search method"
+        type="text"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <br />
+
+      {data.map((exercise) => {
         return (
           <Card className="hpCard">
             <Card.Body className="homeCard">
-              <b>{exercise.name}</b>
-              <p>Exercises:</p>
-              <p>MonkeyMaster:</p>
+              <b className="cardTitle">{exercise.name}</b>
+              <br />
+              Exercises: <br />
+              MonkeyMaster:
             </Card.Body>
           </Card>
         );
       })}
       <Card className="hpCard">
-        <Card.Body className="homeCard">Random</Card.Body>
-      </Card>
-      <Card className="hpCard">
-        <Card.Body className="homeCard">Random</Card.Body>
+        <Card.Body className="homeCard">
+          <b className="cardTitle">Random</b>
+          <br />
+          Exercises: <br />
+          MonkeyMaster:
+        </Card.Body>
       </Card>
     </div>
   );
