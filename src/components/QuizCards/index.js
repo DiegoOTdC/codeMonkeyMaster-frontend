ec238b36e81da51180d4e650b9df457bc5a39122
DiegoOTdC <studioartdiego@gmail.com>
2020-08-26 10:27:48 +0200

    comented out & harcoded data so I can test sending data to QuizCode component

--- src/components/QuizCards/index.js
+++ src/components/QuizCards/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function QuizCards() {
-  return <div></div>;
+  return <div>Quiz card</div>;
 }
--- src/components/QuizCode/index.js
+++ src/components/QuizCode/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function QuizCode() {
-  return <div></div>;
+  return <div>Quiz Code</div>;
 }
